1c2bdb8bbcae2843c7096cbb13f615a3d2762f02
mike011 <mike_ch_1@hotmail.com>
2021-03-02 18:06:35 -0500

    now as a macos app

--- FlashKata/FlashKata/FlashKata.swift
+++
@@ -1,9 +0,0 @@
-//
-//  FlashKata.swift
-//  FlashKata
-//
-//  Created by Michael Charland on 2017-07-19.
-//  Copyright © 2017 charland. All rights reserved.
-//
-
-import Foundation
---
+++ FlashKata/FlashKata/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  FlashKata
+//
+//  Created by Michael Charland on 2021-03-02.
+//
+
+import Foundation
+
+print("Hello, World!")
+
